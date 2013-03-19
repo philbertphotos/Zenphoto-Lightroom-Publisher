741b7711def1196913cdb5ee439c592144e244f0
philbertphotos <joe@philbertphotos.com>
2013-03-19 14:59:54 -0300

    Updated to 3.0.1
    getting ready to add a self update section

--- zp-publisher-lrplugin/ZenphotoPluginInfoProvider.lua
+++ zp-publisher-lrplugin/ZenphotoPluginInfoProvider.lua
@@ -5,10 +5,31 @@ Export service provider description for Lightroom Zenphoto uploader
 
 ------------------------------------------------------------------------------]]
 
+local LrLogger = import 'LrLogger'
+local log = LrLogger( 'ZenphotoLog' )
 -- Zenphoto plugin
 require 'ZenphotoDialogSections'
+--require "LRLib_SelfUpdate"
+local endD = {}
+local endDone = {}
+
+--startD.startDialog = function(startDone)
+  --LRLib_SelfUpdate.startDialog(startDone)
+  --ExportToEmailPlugin.startDialog(startDone)
+--end
+
+
+endD.endDialog = function(endD,endDone)
+  ZenphotoDialogSections.endDialog(endD,endDone)
+  --LRLib_SelfUpdate.endDialog(endD,endDone)
+end
 
 return {
-	startDialog = ZenphotoDialogSections.startDialog,
+startDialog = ZenphotoDialogSections.startDialog,
 	sectionsForTopOfDialog = ZenphotoDialogSections.sectionsForTopOfDialog,
-}
+	
+--sectionsForBottomOfDialog = LRLib_SelfUpdate.PluginInfoSection,
+--sectionsForTopOfDialog = ExportToEmailPlugin.sectionsForTopOfDialog,
+	--startDialog = startD.startDialog, --ExportToEmailPlugin.startDialog,
+	endDialog =  endD.endDialog, --ExportToEmailPlugin.endDialog,
+}	
