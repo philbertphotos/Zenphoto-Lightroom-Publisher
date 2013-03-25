f852cba95b1930b219057b4a1497e606d6c09775
Joseph Philbert <joe@philbertphotos.com>
2013-03-25 11:19:58 -0300

    Create ZenphotoInit.lua

---
+++ zp-publisher-lrplugin/ZenphotoInit.lua
@@ -0,0 +1,10 @@
+--[[----------------------------------------------------------------------------
+
+ZenphotoInit.lua
+Export service provider description for Lightroom Zenphoto uploader
+
+------------------------------------------------------------------------------]]
+
+--require("LRLib_SelfUpdate")
+require("Info")
+--LRLib_SelfUpdate.init(pluginName,"zenphotopublisher",ZenphotoInfo.version, ZenphotoInfo.location , "org.zenphoto.lightroom.publisher")
